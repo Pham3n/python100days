5ab3938cd0496ee153c53fec0ac6561813dd5ab2
pham3n <pham3n@gmail.com>
2023-02-19 18:38:10 +0200

    new file:   100daysdictionaries.py

---
+++ 100daysdictionaries.py
@@ -0,0 +1,9 @@
+# {key : value}
+
+programming_dictionary = {
+    "bug" : "An error that couses program to not run as expected",
+    "loop" : "A method of repeating code multiple times"
+}
+
+#retreaving items from dictionary
+print(programming_dictionary)
